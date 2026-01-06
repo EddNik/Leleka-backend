e17ab1b5baf460078c40edf0236e935aeb42befb
YehorStepanov <stepegorr@gmail.com>
2026-01-06 08:22:26 +0100

    init

---
+++ src/constants/babySex.js
@@ -0,0 +1,5 @@
+export const babySex = [
+  'male',
+  'female',
+  'neutral'
+];
---
+++ src/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import {logger} from './middlewares/logger.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errors } from 'celebrate';
+import { connectMongoDB } from './db/connectMongoDB.js';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(logger);
+app.use(express.json());
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
+
+app.use(cookieParser());
+
+//! приклад як та куди додавати маршрути: app.use(authRoutes);
+
+app.use(notFoundHandler);
+app.use(errors());
+app.use(errorHandler);
+
+await connectMongoDB();
+
+app.listen(PORT, () =>{
+  console.log(`Server is running on port ${PORT}`);
+});
